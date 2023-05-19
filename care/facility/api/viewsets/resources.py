1762717567219f362ff446313dc47f2d400ddec0
Aakash Singh <mail@singhaakash.dev>
2023-05-19 14:43:06 +0530

    fix lint issues

--- care/facility/api/viewsets/resources.py
+++ care/facility/api/viewsets/resources.py
@@ -2,14 +2,17 @@ from django.conf import settings
 from django.db.models.query_utils import Q
 from django_filters import rest_framework as filters
 from djqscsv import render_to_csv_response
-from dry_rest_permissions.generics import DRYPermissionFiltersBase, DRYPermissions
+from dry_rest_permissions.generics import DRYPermissions
 from rest_framework import filters as rest_framework_filters
 from rest_framework import mixins
 from rest_framework.generics import get_object_or_404
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.viewsets import GenericViewSet
 
-from care.facility.api.serializers.resources import ResourceRequestCommentSerializer, ResourceRequestSerializer
+from care.facility.api.serializers.resources import (
+    ResourceRequestCommentSerializer,
+    ResourceRequestSerializer,
+)
 from care.facility.models import (
     RESOURCE_CATEGORY_CHOICES,
     RESOURCE_STATUS_CHOICES,
@@ -57,14 +60,15 @@ def get_request_queryset(request, queryset):
 
 
 class ResourceFilterSet(filters.FilterSet):
-
     status = CareChoiceFilter(choice_dict=inverse_resource_status)
     category = CareChoiceFilter(choice_dict=inverse_category)
     sub_category = CareChoiceFilter(choice_dict=inverse_sub_category)
 
     facility = filters.UUIDFilter(field_name="facility__external_id")
     origin_facility = filters.UUIDFilter(field_name="origin_facility__external_id")
-    approving_facility = filters.UUIDFilter(field_name="approving_facility__external_id")
+    approving_facility = filters.UUIDFilter(
+        field_name="approving_facility__external_id"
+    )
     assigned_facility = filters.UUIDFilter(field_name="assigned_facility__external_id")
     created_date = filters.DateFromToRangeFilter(field_name="created_date")
     modified_date = filters.DateFromToRangeFilter(field_name="modified_date")
@@ -76,7 +80,11 @@ class ResourceFilterSet(filters.FilterSet):
 
 
 class ResourceRequestViewSet(
-    mixins.CreateModelMixin, mixins.ListModelMixin, mixins.RetrieveModelMixin, mixins.UpdateModelMixin, GenericViewSet
+    mixins.CreateModelMixin,
+    mixins.ListModelMixin,
+    mixins.RetrieveModelMixin,
+    mixins.UpdateModelMixin,
+    GenericViewSet,
 ):
     serializer_class = ResourceRequestSerializer
     lookup_field = "external_id"
@@ -103,7 +111,10 @@ class ResourceRequestViewSet(
     ordering_fields = ["id", "created_date", "modified_date", "emergency", "priority"]
 
     permission_classes = (IsAuthenticated, DRYPermissions)
-    filter_backends = (filters.DjangoFilterBackend, rest_framework_filters.OrderingFilter)
+    filter_backends = (
+        filters.DjangoFilterBackend,
+        rest_framework_filters.OrderingFilter,
+    )
     filterset_class = ResourceFilterSet
 
     def get_queryset(self):
@@ -111,7 +122,9 @@ class ResourceRequestViewSet(
 
     def list(self, request, *args, **kwargs):
         if settings.CSV_REQUEST_PARAMETER in request.GET:
-            queryset = self.filter_queryset(self.get_queryset()).values(*ResourceRequest.CSV_MAPPING.keys())
+            queryset = self.filter_queryset(self.get_queryset()).values(
+                *ResourceRequest.CSV_MAPPING.keys()
+            )
             return render_to_csv_response(
                 queryset,
                 field_header_map=ResourceRequest.CSV_MAPPING,
@@ -121,7 +134,10 @@ class ResourceRequestViewSet(
 
 
 class ResourceRequestCommentViewSet(
-    mixins.CreateModelMixin, mixins.ListModelMixin, mixins.RetrieveModelMixin, GenericViewSet
+    mixins.CreateModelMixin,
+    mixins.ListModelMixin,
+    mixins.RetrieveModelMixin,
+    GenericViewSet,
 ):
     serializer_class = ResourceRequestCommentSerializer
     lookup_field = "external_id"
@@ -130,19 +146,31 @@ class ResourceRequestCommentViewSet(
     permission_classes = (IsAuthenticated,)
 
     def get_queryset(self):
-        queryset = self.queryset.filter(request__external_id=self.kwargs.get("resource_external_id"))
+        queryset = self.queryset.filter(
+            request__external_id=self.kwargs.get("resource_external_id")
+        )
         if self.request.user.is_superuser:
             pass
         else:
             if self.request.user.user_type >= User.TYPE_VALUE_MAP["StateLabAdmin"]:
                 q_objects = Q(request__origin_facility__state=self.request.user.state)
-                q_objects |= Q(request__approving_facility__state=self.request.user.state)
-                q_objects |= Q(request__assigned_facility__state=self.request.user.state)
+                q_objects |= Q(
+                    request__approving_facility__state=self.request.user.state
+                )
+                q_objects |= Q(
+                    request__assigned_facility__state=self.request.user.state
+                )
                 return queryset.filter(q_objects)
             elif self.request.user.user_type >= User.TYPE_VALUE_MAP["DistrictLabAdmin"]:
-                q_objects = Q(request__origin_facility__district=self.request.user.district)
-                q_objects |= Q(request__approving_facility__district=self.request.user.district)
-                q_objects |= Q(request__assigned_facility__district=self.request.user.district)
+                q_objects = Q(
+                    request__origin_facility__district=self.request.user.district
+                )
+                q_objects |= Q(
+                    request__approving_facility__district=self.request.user.district
+                )
+                q_objects |= Q(
+                    request__assigned_facility__district=self.request.user.district
+                )
                 return queryset.filter(q_objects)
             facility_ids = get_accessible_facilities(self.request.user)
             q_objects = Q(request__origin_facility__id__in=facility_ids)
@@ -158,4 +186,3 @@ class ResourceRequestCommentViewSet(
 
     def perform_create(self, serializer):
         serializer.save(request=self.get_request())
-
